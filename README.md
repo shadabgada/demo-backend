6dd094bf40067e1af762ce8d286debdd4cf8d1fb
Shadab <shadabgada234@gmail.com>
2021-08-08 16:24:05 +0530

    updated yaml file and CORS code

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # demo-backend
+
+docker build -t demo:1.0 .
+
+docker run -p 8080:8080 demo
+
--- src/main/java/com/example/demobackend/controller/TestController.java
+++ src/main/java/com/example/demobackend/controller/TestController.java
@@ -1,16 +1,24 @@
 package com.example.demobackend.controller;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
+//@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class TestController {
 
-
     @GetMapping
-    public String getMessage(){
-        return "Hello " + System.getenv("name");
+    public ResponseEntity getMessage(){
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_TYPE, "application/json; charset=UTF-8");
+
+        return ResponseEntity.ok()
+                .body("Hello " + System.getenv("name"));
     }
 }
